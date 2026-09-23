89d4fc724ae70dab184e8d50ad509a01ce7c9904
yoav123453 <182245856+yoav123453@users.noreply.github.com>
2026-09-23 12:13:20 +0000

    Add tests for activity signup and unregistration functionality

---
+++ tests/conftest.py
@@ -0,0 +1,13 @@
+from copy import deepcopy
+
+import pytest
+
+from src import app as app_module
+
+
+@pytest.fixture(autouse=True)
+def restore_activities():
+    original_activities = deepcopy(app_module.activities)
+    yield
+    app_module.activities.clear()
+    app_module.activities.update(original_activities)
